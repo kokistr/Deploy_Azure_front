ad55ca19aae92a8eea99fdcaa7b3fd538bab3682
kokistr <125700374+kokistr@users.noreply.github.com>
2024-11-21 14:23:41 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 
 export default function Home() {
-
-  //GETリクエストを送信
+  // GETリクエストを送信
   const [getResponse, setGetResponse] = useState('');
 
   const handleGetRequest = async () => {
-    const res = await fetch('http://localhost:5000/api/hello', {
+    const res = await fetch('https://techo-gen-8-step3-testapp-py2-28.azurewebsites.net/api/hello', {
       method: 'GET',
     });
     const data = await res.json();
-
 
     // GETリクエストの結果をコンソールに表示
     console.log("GETリクエストの結果:", data.message);
@@ -18,15 +16,14 @@ export default function Home() {
     setGetResponse(data.message);
   };
 
-  //動的なGETリクエストの送信
+  // 動的なGETリクエストの送信
   const [id, setId] = useState('');
   const [idResponse, setIdResponse] = useState('');
 
-  // IDを指定してGETリクエストを送信
   const handleIdRequest = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`http://localhost:5000/api/multiply/${id}`, {
+    const res = await fetch(`https://techo-gen-8-step3-testapp-py2-28.azurewebsites.net/api/multiply/${id}`, {
       method: 'GET',
     });
     const data = await res.json();
@@ -37,37 +34,34 @@ export default function Home() {
     setIdResponse(data.doubled_value);
   };
 
-  //POSTリクエストを送信
+  // POSTリクエストを送信
   const [input, setInput] = useState('');
   const [postResponse, setPostResponse] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    //入力されたデータをコンソールに表示
+    // 入力されたデータをコンソールに表示
     console.log("入力情報:", input);
 
-    const res = await fetch('http://localhost:5000/api/echo', {
+    const res = await fetch('https://techo-gen-8-step3-testapp-py2-28.azurewebsites.net/api/echo', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ "message":input }),
-
+      body: JSON.stringify({ "message": input }),
     });
-    console.log(JSON.stringify({ "message":input }));
+    console.log(JSON.stringify({ "message": input }));
     const data = await res.json();
 
-    //バックエンドからのレスポンスをコンソールに表示
+    // バックエンドからのレスポンスをコンソールに表示
     console.log("Backendからのお返事:", data.message);
 
     setPostResponse(data.message);
   };
 
-
   return (
     <div>
-
       <h1>Next.jsとFlaskの連携アプリ</h1>
 
       <h2>GETリクエストを送信</h2>
@@ -91,15 +85,12 @@ export default function Home() {
         <input
           type="text"
           value={input}
-
           onChange={(e) => setInput(e.target.value)}
           placeholder="テキストを入力してください"
         />
-
         <button type="submit">送信</button>
       </form>
       {postResponse && <p>FlaskからのPOST応答: {postResponse}</p>}
-
     </div>
   );
 }
