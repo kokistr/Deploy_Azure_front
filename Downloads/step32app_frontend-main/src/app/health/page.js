9d8b172d60f74f878182d9907c942c0e1f8c0f83
羽田野公輝 <hadanokoki@hatanokinterunoMacBook-Pro.local>
2024-12-16 22:27:47 +0900

    change

--- Downloads/step32app_frontend-main/src/app/health/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function Home(){
-  return(
-    <main>
-      <h1 className="m-10">Healthのトップ ページをここに記述</h1>
-    </main>
-  )
-}
--- Downloads/step32app_frontend-main/src/app/shopping/order/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function Home(){
-  return(
-    <main>
-      <h1 className="m-10">注文ページをここに記述。ここには、注文ページが作成される</h1>
-    </main>
-  )
-}
